ca918d09f0245893d5474de1b1b4df688b80b6ee
Kenchir <kchirchir.dev@gmail.com>
2021-03-08 17:01:56 +0300

    update: mongodb atlas database

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -7,8 +7,8 @@ const config = {
   url: '',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: '',
-  password: '',
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   useNewUrlParser: true,
 };
